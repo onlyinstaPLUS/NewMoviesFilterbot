5b474b90570735762901f131738fa127d8106609
onlyinstaPLUS <134713123+onlyinstaPLUS@users.noreply.github.com>
2023-12-01 20:48:52 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -1,6 +1,6 @@
-# Don't Remove Credit @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit @Official_InstaPLUS
+# Subscribe YouTube Channel For Amazing Bot @Insta_PLUS
+# Ask Doubt on telegram @HackerPushkar
 
 import logging
 from pyrogram import Client, emoji, filters
